5a45044a733e07690562a8468b555a229a380917
straaaa <kame1234kenta@yahoo.co.jp>
2022-06-04 06:41:10 +0900

    Initial commit

---
+++ AstralGamePayload/src/main/kotlin/io/github/straaaa/astralgamepayload/Main.kt
@@ -0,0 +1,9 @@
+package io.github.straaaa.astralgamepayload
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main: JavaPlugin() {
+    override fun onEnable() {
+        println("Hello World! ^^")
+    }
+}
